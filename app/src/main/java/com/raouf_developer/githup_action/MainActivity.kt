6f67577818bb2238e350571d0ad4af2bd805fe70
raouf-kamel <raoufkamel2013@gmail.com>
2025-10-22 23:05:10 +0300

    feat: Update greeting text
    The greeting text in the main activity has been updated from "Android v1.2.3-test to 1.3.3" to "Android v1.2.3-test to 231".

--- app/src/main/java/com/raouf_developer/githup_action/MainActivity.kt
+++ app/src/main/java/com/raouf_developer/githup_action/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GithupactionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android v1.2.3-test to 1.3.3",
+                        name = "Android v1.2.3-test to 231",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
